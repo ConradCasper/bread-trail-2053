29ccb2d94076be42bfbef9941c36febfa22aebd5
github username <github email address>
2019-07-11 17:35:05 -0400

    login and new user working

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,12 @@ Rails.application.routes.draw do
   resources :categories
   resources :users
   resources :posts
+  resources :sessions, only: [:new, :create]
+
+  root 'sessions#new'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
