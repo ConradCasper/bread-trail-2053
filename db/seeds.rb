211e9eca925ed0ea2357a06b8f1ccb291a6e13b5
github username <github email address>
2019-07-11 17:46:17 -0400

    finished relationships for post

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Like.destroy_all
+Comment.destroy_all
+Category.destroy_all
+Post.destroy_all
+User.destroy_all
+
